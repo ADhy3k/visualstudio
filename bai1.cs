6dddeb11b0b9d7ea16af13bba5efc79a751d22b2
Anh Duy <anhduykg3k@gmail.com>
2023-08-23 16:06:05 +0700

    add file bai1

---
+++ bai1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2033210434_LDAD_Bai1
+{
+    class Bai1
+    {
+        int x,y;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        char tenTD;
+
+        public char TenTD
+        {
+          get { return tenTD; }
+          set { tenTD = value; }
+        }
+                public Bai1()
+                {}
+                public Bai1(char tenTD, int x, int y)
+                {
+                    TenTD = this.tenTD;
+                    X = this.x;
+                    Y = this.y;
+                }
+                public void nhap(){
+                    Console.WriteLine("Nhap ten toa do: ");
+                    TenTD = char.Parse(Console.ReadLine());
+                    Console.WriteLine("Nhap x, y: ");
+                    X = int.Parse(Console.ReadLine());
+                    Y = int .Parse(Console.ReadLine());
+                    Console.WriteLine("Toa do: {0}({0},{0}) ",TenTD,X,Y);
+                }
+    }
+}
